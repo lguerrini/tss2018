3e7ebfa5d0fc73491a96268d4e30d715621ba0fa
tss <tss@debian>
2018-12-11 12:16:34 +0100

    prima oprova 2 coomit

--- public_html/js/myjs.js
+++ public_html/js/myjs.js
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 
+import Messagge from "./message.js";
 
-function myf(){
-    
-    x=document.querySelector("#mydiv");
-    x.innerHTML="qweqwe";
-    
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", onclick);
+
+function onclick(e) {
+    console.log("onclick");
+    let m = new Messagge("luca", "ciao");
+    console.log(m.sender);
 }
+
+
+/*
+ export default function myf(){
+ 
+ x=document.querySelector("#mydiv");
+ x.innerHTML="qweqwe";
+ 
+ }
+ */
 
